f7165a6477a26e0005f8d440b5b820f23c94a17f
Tod E. Kurt <tod@todbot.com>
2013-01-04 12:01:03 -0800

    win app: url parsing (and thus file & script parsing) looks to be done, still havent figured out settings with data structs

--- windows/Blink1Control/Blink1Control/Blink1Input.cs
+++ windows/Blink1Control/Blink1Control/Blink1Input.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
+//using System.Runtime.Serialization;
 
 namespace Blink1Control
 {
-    [Serializable]
+//    [Serializable]
     public class Blink1Input
     {
         /// <summary>
