08597523e6b10e917aa0fa9174616ff87c553fc4
Tod E. Kurt <tod@todbot.com>
2013-01-13 00:50:51 -0800

    win app: tray icon more fleshed out

--- windows/Blink1Control/Blink1Control/Form1.Designer.cs
+++ windows/Blink1Control/Blink1Control/Form1.Designer.cs
@@ -31,6 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webControl1 = new Awesomium.Windows.Forms.WebControl();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stripMenuBlink1Status = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuBlink1Id = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuResetAlerts = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuKey = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webControl1
@@ -46,11 +54,61 @@
             // 
             // notifyIcon1
             // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "Blink1Control";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripMenuBlink1Status,
+            this.stripMenuBlink1Id,
+            this.stripMenuKey,
+            this.toolStripSeparator1,
+            this.stripMenuResetAlerts,
+            this.stripMenuExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 142);
+            // 
+            // stripMenuBlink1Status
+            // 
+            this.stripMenuBlink1Status.Name = "stripMenuBlink1Status";
+            this.stripMenuBlink1Status.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuBlink1Status.Text = "blink1Status";
+            // 
+            // stripMenuBlink1Id
+            // 
+            this.stripMenuBlink1Id.Name = "stripMenuBlink1Id";
+            this.stripMenuBlink1Id.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuBlink1Id.Text = "serial";
+            // 
+            // stripMenuExit
+            // 
+            this.stripMenuExit.Name = "stripMenuExit";
+            this.stripMenuExit.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuExit.Text = "Exit";
+            this.stripMenuExit.Click += new System.EventHandler(this.stripMenuExit_Click);
+            // 
+            // stripMenuResetAlerts
+            // 
+            this.stripMenuResetAlerts.Name = "stripMenuResetAlerts";
+            this.stripMenuResetAlerts.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuResetAlerts.Text = "Reset Alerts";
+            this.stripMenuResetAlerts.Click += new System.EventHandler(this.stripMenuResetAlerts_Click);
+            // 
+            // stripMenuKey
+            // 
+            this.stripMenuKey.Name = "stripMenuKey";
+            this.stripMenuKey.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuKey.Text = "key";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
             // 
             // Form1
             // 
@@ -60,7 +118,9 @@
             this.Controls.Add(this.webControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -69,6 +129,13 @@
 
         private Awesomium.Windows.Forms.WebControl webControl1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuBlink1Status;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuBlink1Id;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuExit;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuResetAlerts;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuKey;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
 
--- windows/Blink1Control/Blink1Control/Form1.cs
+++ windows/Blink1Control/Blink1Control/Form1.cs
@@ -43,29 +43,41 @@ namespace Blink1Control
         private void notifyIcon1_Click(object sender, EventArgs e)
         {
             Console.WriteLine("notifyIcon1 SingleClick! "+e);
+            stripMenuBlink1Status.Text = "blink1 status: hello";
+            if (blink1Server.blink1.getCachedCount() > 0) {
+                stripMenuBlink1Status.Text = "blink(1) found";
+                stripMenuBlink1Id.Text = "serial: " + blink1Server.blink1.getCachedSerial(0);
+                stripMenuKey.Text = "key: "+ blink1Server.blink1.blink1Id;
+            }
+            else {
+                stripMenuBlink1Status.Text = "blink(1) not found";
+                stripMenuBlink1Id.Text = "serial: -none-";
+                stripMenuKey.Text = "key: " + blink1Server.blink1.blink1Id;
+            }
         }
 
+        private void stripMenuResetAlerts_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void stripMenuExit_Click(object sender, EventArgs e)
+        {
+            Close();
+            blink1Server.shutdown();
+            System.Windows.Forms.Application.Exit();
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Console.WriteLine("FormClosed!");
+        }
         private void Awesomium_Windows_Forms_WebControl_Resize(object sender, EventArgs e)
         {
             Console.WriteLine("HELLO THERE");
         }
 
-        /*
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            if (disposeTime) {
-                //close it
-                blink1Server.shutdown();
-                // FIXME: is this the right thing to do?
-                System.Windows.Forms.Application.Exit();
-                System.Diagnostics.Process.GetCurrentProcess().Kill();
-            }
-            else {
-                base.OnClosing(e);
-                e.Cancel = true;
-                this.WindowState = WindowState.Minimized;
-            }
-        }
-         */
+
+
     }
 }
