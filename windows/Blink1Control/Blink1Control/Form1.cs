1d57be8cfc67779bb6409c9a21dd7f381d0d2724
Tod E. Kurt <tod@todbot.com>
2013-01-13 18:29:23 -0800

    win app: moved from awesomium to cefsharp, works better wrt HTML SELECT

--- windows/Blink1Control/Blink1Control/Form1.cs
+++ windows/Blink1Control/Blink1Control/Form1.cs
@@ -7,16 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using CefSharp;
+using CefSharp.WinForms;
+
 
 namespace Blink1Control
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form,IRequestHandler
     {
         Blink1Server blink1Server = new Blink1Server();
+
+        private readonly WebView web_view;
 
         public Form1()
         {
             InitializeComponent();
+
+            BrowserSettings bs = new BrowserSettings();
+            Console.WriteLine("BrowserSettings: " + bs);
+            bs.WebGlDisabled = true;
+            bs.PluginsDisabled = true;
+            //web_view = new WebView("http://stackoverflow.com", bs);
+            web_view = new WebView("http://127.0.0.1:8934/blink_1/", bs);
+            web_view.Dock = DockStyle.Fill;
+            web_view.RequestHandler = this;
+            this.Controls.Add(web_view);
+            //containerControl1.Controls.Add(web_view);
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -84,5 +101,30 @@ namespace Blink1Control
             System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
 
+        #region IRequestHandler Members
+
+        bool IRequestHandler.OnBeforeBrowse(IWebBrowser browser, IRequest request,
+                                   NavigationType naigationvType, bool isRedirect)
+        {
+            //System.Diagnostics.Debug.WriteLine("OnBeforeBrowse");
+            return false;
+        }
+
+        bool IRequestHandler.OnBeforeResourceLoad(IWebBrowser browser,
+                                    IRequestResponse requestResponse)
+        {
+            //System.Diagnostics.Debug.WriteLine("OnBeforeResourceLoad");
+            //IRequest request = requestResponse.Request;
+            return false;
+        }
+
+        void IRequestHandler.OnResourceResponse(IWebBrowser browser, string url,
+                                       int status, string statusText,
+                                       string mimeType, WebHeaderCollection headers)
+        {
+            //System.Diagnostics.Debug.WriteLine("OnResourceResponse");
+        }
+
+        #endregion
     }
 }
