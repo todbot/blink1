29ec8e10f78ff332fa5c8fde2d68bf17903a9a50
Tod E. Kurt <tod@todbot.com>
2013-01-13 18:29:23 -0800

    win app: moved from awesomium to cefsharp, works better wrt HTML SELECT

--- windows/Blink1Control/Blink1Control/Form1.Designer.cs
+++ windows/Blink1Control/Blink1Control/Form1.Designer.cs
@@ -29,28 +29,16 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.webControl1 = new Awesomium.Windows.Forms.WebControl();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.stripMenuBlink1Status = new System.Windows.Forms.ToolStripMenuItem();
             this.stripMenuBlink1Id = new System.Windows.Forms.ToolStripMenuItem();
-            this.stripMenuExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.stripMenuResetAlerts = new System.Windows.Forms.ToolStripMenuItem();
             this.stripMenuKey = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.stripMenuResetAlerts = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuExit = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // webControl1
-            // 
-            this.webControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webControl1.Location = new System.Drawing.Point(0, 0);
-            this.webControl1.Name = "webControl1";
-// TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
-            this.webControl1.Size = new System.Drawing.Size(839, 617);
-            this.webControl1.Source = new System.Uri("http://127.0.0.1:8934/blink_1", System.UriKind.Absolute);
-            this.webControl1.TabIndex = 0;
-            this.webControl1.Resize += new System.EventHandler(this.Awesomium_Windows_Forms_WebControl_Resize);
             // 
             // notifyIcon1
             // 
@@ -76,48 +64,48 @@
             // stripMenuBlink1Status
             // 
             this.stripMenuBlink1Status.Name = "stripMenuBlink1Status";
-            this.stripMenuBlink1Status.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuBlink1Status.Size = new System.Drawing.Size(138, 22);
             this.stripMenuBlink1Status.Text = "blink1Status";
             // 
             // stripMenuBlink1Id
             // 
             this.stripMenuBlink1Id.Name = "stripMenuBlink1Id";
-            this.stripMenuBlink1Id.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuBlink1Id.Size = new System.Drawing.Size(138, 22);
             this.stripMenuBlink1Id.Text = "serial";
-            // 
-            // stripMenuExit
-            // 
-            this.stripMenuExit.Name = "stripMenuExit";
-            this.stripMenuExit.Size = new System.Drawing.Size(152, 22);
-            this.stripMenuExit.Text = "Exit";
-            this.stripMenuExit.Click += new System.EventHandler(this.stripMenuExit_Click);
-            // 
-            // stripMenuResetAlerts
-            // 
-            this.stripMenuResetAlerts.Name = "stripMenuResetAlerts";
-            this.stripMenuResetAlerts.Size = new System.Drawing.Size(152, 22);
-            this.stripMenuResetAlerts.Text = "Reset Alerts";
-            this.stripMenuResetAlerts.Click += new System.EventHandler(this.stripMenuResetAlerts_Click);
             // 
             // stripMenuKey
             // 
             this.stripMenuKey.Name = "stripMenuKey";
-            this.stripMenuKey.Size = new System.Drawing.Size(152, 22);
+            this.stripMenuKey.Size = new System.Drawing.Size(138, 22);
             this.stripMenuKey.Text = "key";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(135, 6);
+            // 
+            // stripMenuResetAlerts
+            // 
+            this.stripMenuResetAlerts.Name = "stripMenuResetAlerts";
+            this.stripMenuResetAlerts.Size = new System.Drawing.Size(138, 22);
+            this.stripMenuResetAlerts.Text = "Reset Alerts";
+            this.stripMenuResetAlerts.Click += new System.EventHandler(this.stripMenuResetAlerts_Click);
+            // 
+            // stripMenuExit
+            // 
+            this.stripMenuExit.Name = "stripMenuExit";
+            this.stripMenuExit.Size = new System.Drawing.Size(138, 22);
+            this.stripMenuExit.Text = "Exit";
+            this.stripMenuExit.Click += new System.EventHandler(this.stripMenuExit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(839, 617);
-            this.Controls.Add(this.webControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Blink1Control";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuStrip1.ResumeLayout(false);
@@ -127,7 +115,6 @@
 
         #endregion
 
-        private Awesomium.Windows.Forms.WebControl webControl1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem stripMenuBlink1Status;
