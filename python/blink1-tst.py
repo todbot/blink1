fd71904217f994164e3119f49163cdd9ea607439
Tod E. Kurt <tod@todbot.com>
2013-08-30 22:36:14 -0700

    more python library work

--- python/blink1-tst.py
+++ python/blink1-tst.py
@@ -1,16 +1,16 @@
 
-import blink1
+from blink1 import Blink1
 import time
 
-blink1 = blink1.Blink1()
+blink1 = Blink1()
 
 if( blink1.dev == None ):
     print("no blink1 found")
 
-blink1.fadeToRGB(   255,255,255, 1000, 0 )
+blink1.fade_to_rgb(   255,255,255, 1000, 0 )
 
 time.sleep(0.5)
         
-blink1.fadeToRGB(   0,0,0, 1000, 0 )
+blink1.fade_to_rgb(   0,0,0, 1000, 0 )
         
 
