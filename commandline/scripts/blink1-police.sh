c424fc8f68736e4944a2c8d12068aac6e8042db2
Tod E. Kurt <tod@todbot.com>
2014-06-19 20:12:49 -0700

    fixed example scripts

--- commandline/scripts/blink1-police.sh
+++ commandline/scripts/blink1-police.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-
+# Act like a police light
+# for mk2 devices
+#
 while [ 1 ] ; do
-blink1-tool -l 2 --red && blink1-tool -l 1 --blue
-sleep 1
-blink1-tool -l 1 --red && blink1-tool -l 2 --rgb 255,255,40
-sleep 1
+  blink1-tool -l 2 --red && blink1-tool -l 1 --blue
+  sleep 0.5
+  blink1-tool -l 1 --red && blink1-tool -l 2 --blue
+  sleep 0.5
 done
