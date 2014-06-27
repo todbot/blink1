688ef24835b3b84e077710737aa430160b80b326
Salim Fadhley <sal@stodge.org>
2014-06-27 01:05:03 +0100

    add the color correction feature

--- python/pypi/blink1/shine.py
+++ python/pypi/blink1/shine.py
@@ -4,8 +4,9 @@ from blink1.blink1 import blink1
 
 @click.command()
 @click.option('--color', default='white', help='What colour to set the Blink(1)')
-def shine(color):
-    with blink1(False) as b1:
+@click.option('--white_point', default='incandescent', help='The name of the white-point to use')
+def shine(color, white_point):
+    with blink1(switch_off=False, white_point=white_point) as b1:
         b1.fade_to_color(100, color)
 
 
