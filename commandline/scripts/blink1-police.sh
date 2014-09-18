5ab4016df11232020b57d895eb40c2087f62bf89
Tod E. Kurt <tod@todbot.com>
2014-09-17 23:27:55 -0700

    blink1control: multi-blink1 via API server working really well, maybe just maybe

--- commandline/scripts/blink1-police.sh
+++ commandline/scripts/blink1-police.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+#
 # Act like a police light
 # for mk2 devices
 #
 while [ 1 ] ; do
-  blink1-tool -l 2 --red && blink1-tool -l 1 --blue
+  blink1-tool -l 2 --red 
+  blink1-tool -l 1 --blue
   sleep 0.5
-  blink1-tool -l 1 --red && blink1-tool -l 2 --blue
+  blink1-tool -l 1 --red
+  blink1-tool -l 2 --blue
   sleep 0.5
 done
